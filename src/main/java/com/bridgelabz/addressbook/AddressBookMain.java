49ba53b787ce5c61f008089334750bc8b61ebaaa
prashant <pkapde308@gmail.com>
2021-03-30 23:48:10 +0530

    Refactor:Edited the address book for adding no duplicate entry

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,52 +1,91 @@
 package com.bridgelabz.addressbook;
 import java.util.*;
-
+import java.util.stream.Collectors;
 public class AddressBookMain {
-    Contact contact;
-    //Map.Entry[] contents;
-    //private static List<Contact> Contact = new ArrayList<Contact>();
-    public static void main(String[] args) {
-        System.out.println("Welcome to address book");
+    private static Map<String, ArrayList<Contact>> bookList = new HashMap<String, ArrayList<Contact>>();
+    private static List<String> duplicateCheck;
+    private static Contact getContactDetails() {
+        System.out.println("enter contact details ----");
+        Scanner scan = new Scanner(System.in);
 
-        AddressBookMain addressBookMain = new AddressBookMain();
-        addressBookMain.contact = addressBookMain.getUserInput();
-        //UC3_EditEntry entry3 = new UC3_EditEntry();
-        //addressBookMain.contact = addressBookMain.editEntry();
-        //addressBookMain.contact = addressBookMain.deleteByFirstName();
-        System.out.println("new Contact---->" + addressBookMain.contact);
+        System.out.println("enter first name");
+        String firstName = scan.nextLine();
 
+        System.out.println("enter last name");
+        String lastName = scan.nextLine();
+
+        System.out.println("enter street address");
+        String address = scan.nextLine();
+
+        System.out.println("enter city name");
+        String cityName = scan.nextLine();
+
+        System.out.println("enter state name");
+        String stateName = scan.nextLine();
+
+        System.out.println("enter phone number");
+        String phoneNumber = scan.nextLine();
+
+        System.out.println("enter email Address");
+        String emailAddress = scan.nextLine();
+
+        System.out.println("enter zip code ");
+        String zipCode = scan.nextLine();
+
+        Contact entry = new Contact(firstName, lastName,address, cityName, stateName, zipCode, phoneNumber,
+                emailAddress);
+        return entry;
     }
+    private static void getAddressBook() {
+        ArrayList<Contact> contactDetails = new ArrayList<Contact>();
+        Map<String, ArrayList<Contact>> bookListLocal = new HashMap<String, ArrayList<Contact>>();
+        ArrayList<String> names = new ArrayList<String>();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter address book name");
+        String bookname = scan.nextLine();
+        while (true) {
+            System.out.println("enter 1 to add more contact");
+            System.out.println("enter 2 to exit ");
+            int option = scan.nextInt();
+            if (option == 1) {
+                Contact contactEntry = getContactDetails();
+                String fullName = contactEntry.getFirstName() + " " + contactEntry.getLastName();
+                // using stream to check duplicates
+                duplicateCheck = names.stream().filter(n -> n.equals(fullName)).collect(Collectors.toList());
 
-
-
-    private Contact getUserInput() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter fist name:");
-        String firstName = scanner.nextLine();
-        System.out.println("Enter last name:");
-        String lastName = scanner.nextLine();
-        System.out.println("Enter phone number:");
-        String phoneNumber = scanner.nextLine();
-        System.out.println("Enter city:");
-        String city = scanner.nextLine();
-        System.out.println("Enter state:");
-        String state = scanner.nextLine();
-        System.out.println("Enter email:");
-        String email = scanner.nextLine();
-        System.out.println("Enter address:");
-        String address = scanner.nextLine();
-        System.out.println("Enter zip:");
-        String zip = scanner.nextLine();
-        //return contact;
-        return new Contact(firstName, lastName, phoneNumber,city,state,email,address,zip);
-    }
-    public boolean createContact(Contact contact) {
-        this.contact = contact;
-        if(this.contact == null){
-            return false;
+                if (duplicateCheck.size() == 0) {
+                    contactDetails.add(contactEntry);
+                    names.add(contactEntry.getFirstName() + " " + contactEntry.getLastName());
+                } else {
+                    System.out.println("this name already exists , not added to book");
+                }
+            } else {
+                break;
+            }
         }
-        return true;
+        bookList.put(bookname, contactDetails);
     }
-
-
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("this is address book program for no duplicate entry");
+        while (true) {
+            System.out.println("enter 1 to add more address books");
+            System.out.println("enter 2 to exit from address book program");
+            int option = scn.nextInt();
+            if (option == 1) {
+                getAddressBook();
+            } else {
+                break;
+            }
+        }
+        System.out.println("address books and contact details added successfully ");
+        for (String i : bookList.keySet()) {
+            System.out.println("book name is " + i);
+            System.out.println("contact names are ");
+            ArrayList<Contact> persons = bookList.get(i);
+            for (int y = 0; y < persons.size(); y++) {
+                System.out.println(persons.get(y).getFirstName() + " " + persons.get(y).getLastName());
+            }
+        }
+    }
 }
