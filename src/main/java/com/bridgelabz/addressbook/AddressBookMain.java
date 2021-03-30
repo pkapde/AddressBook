f90e4080c5507d942397c41eee56220526bf8f2c
prashant <pkapde308@gmail.com>
2021-03-30 20:30:49 +0530

    Refactor:Edited the address book main for multiple contacts entry

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,52 +1,85 @@
 package com.bridgelabz.addressbook;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class AddressBookMain {
-    Contact contact;
-    //Map.Entry[] contents;
-    //private static List<Contact> Contact = new ArrayList<Contact>();
-    public static void main(String[] args) {
-        System.out.println("Welcome to address book");
+class AddressBookMain {
+    private ArrayList<Contact> contactList = new ArrayList<>();
 
-        AddressBookMain addressBookMain = new AddressBookMain();
-        addressBookMain.contact = addressBookMain.getUserInput();
-        //UC3_EditEntry entry3 = new UC3_EditEntry();
-        //addressBookMain.contact = addressBookMain.editEntry();
-        //addressBookMain.contact = addressBookMain.deleteByFirstName();
-        System.out.println("new Contact---->" + addressBookMain.contact);
+    private static Contact getContactDetails() {
+        System.out.println("enter contact details ----");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("enter first name");
+        String firstName = scan.nextLine();
+
+        System.out.println("enter last name");
+        String lastName = scan.nextLine();
+
+        System.out.println("enter street address");
+        String address = scan.nextLine();
+
+        System.out.println("enter city name");
+        String cityName = scan.nextLine();
+
+        System.out.println("enter state name");
+        String stateName = scan.nextLine();
+
+        System.out.println("enter phone number");
+        String phoneNumber = scan.nextLine();
+
+        System.out.println("enter email Address");
+        String emailAddress = scan.nextLine();
+
+        System.out.println("enter zip code ");
+        String zipCode = scan.nextLine();
+
+        Contact entry = new Contact(firstName, lastName,address, cityName, stateName, zipCode, phoneNumber,
+                emailAddress);
+        return entry;
 
     }
 
+    private void addContact(Contact contact) {
+        contactList.add(contact);
+        System.out.println("contact added whose name is :  " + contact.getFirstName() + " " + contact.getLastName());
 
-
-    private Contact getUserInput() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter fist name:");
-        String firstName = scanner.nextLine();
-        System.out.println("Enter last name:");
-        String lastName = scanner.nextLine();
-        System.out.println("Enter phone number:");
-        String phoneNumber = scanner.nextLine();
-        System.out.println("Enter city:");
-        String city = scanner.nextLine();
-        System.out.println("Enter state:");
-        String state = scanner.nextLine();
-        System.out.println("Enter email:");
-        String email = scanner.nextLine();
-        System.out.println("Enter address:");
-        String address = scanner.nextLine();
-        System.out.println("Enter zip:");
-        String zip = scanner.nextLine();
-        //return contact;
-        return new Contact(firstName, lastName, phoneNumber,city,state,email,address,zip);
     }
-    public boolean createContact(Contact contact) {
-        this.contact = contact;
-        if(this.contact == null){
-            return false;
+
+    private void displayContactDetails() {
+        System.out.println("displaying contact details :");
+        for (int i = 0; i < contactList.size(); i++) {
+            System.out.println("contact no" + (i + 1));
+            Contact con = contactList.get(i);
+            System.out.println("first name is :" + con.getFirstName());
+
+            System.out.println(" last name is :" + con.getLastName());
+
+            System.out.println(" address is :" + con.getAddress());
+
+            System.out.println(" city name is :" + con.getCity());
+
+            System.out.println(" state name is :" + con.getState());
+
+            System.out.println(" zip code is  :" + con.getZip());
+
+            System.out.println(" phone number is :" + con.getPhone());
+
+            System.out.println(" email address is :" + con.getEmail());
         }
-        return true;
     }
-
-
+    public static void main(String[] args) {
+        System.out.println("this is address book feature to add multiple contacts");
+        AddressBookMain entry = new AddressBookMain();
+        System.out.println("Enter  no of contacts you wants to add");
+        Scanner scn = new Scanner(System.in);
+        int contactNumber = scn.nextInt();
+        for (int k = 0; k < contactNumber; k++) {
+            System.out.println("Enter new contact details ");
+            entry.addContact(entry.getContactDetails());
+        }
+        System.out.println("contact book after addition");
+        entry.displayContactDetails();
+    }
 }
+
+
